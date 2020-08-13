34e7eab559f355a317974afd0af3e9487226833f
larry laconi <soverylarry@gmail.com>
2020-08-12 20:16:52 -0400

    adding all the extra homework files and author request files

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = new express()
+
+app.use(express.static('public'))
+
+
+app.listen(4000, ()=>{
+    console.log ('App listening to your Mom on port 4000')
+})
